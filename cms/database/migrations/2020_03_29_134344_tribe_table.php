dd3eef977d8975a34b1d0a167ecbbdc0d4e2b0b0
yudai-dayo <b_st_yudai@yahoo.co.jp>
2020-03-29 22:51:27 +0900

    試し

--- cms/database/migrations/2020_03_29_134344_tribe_table.php
+++ cms/database/migrations/2020_03_29_134344_tribe_table.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Migrations\Migration;
 class TribeTable extends Migration
 {
     /**
-     * Run the migrations.
+     * Run the migrations...
      *
      * @return void
      */
